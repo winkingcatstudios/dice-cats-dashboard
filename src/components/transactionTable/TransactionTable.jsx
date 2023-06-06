3324811f4fb2dd26fc0e2b9293e3e4fd42f26cf6
winkingcat <dankercher@gmail.com>
2023-06-06 15:00:28 -0400

    add table

--- src/components/transactionTable/TransactionTable.jsx
+++ src/components/transactionTable/TransactionTable.jsx
@@ -11,12 +11,66 @@ import "./transactionTable.scss";
 
 const TransactionTable = () => {
   const rows = [
-    {id: 12345, product: "Product A", img: "", customer: "Bob", date: "1 March", amount: 765, method: "COD", status: "Approved"},
-    {id: 12346, product: "Product B", img: "", customer: "Bob", date: "2 March", amount: 766, method: "COD", status: "Approved"},
-    {id: 12347, product: "Product C", img: "", customer: "Bob", date: "3 March", amount: 767, method: "COD", status: "Approved"},
-    {id: 12348, product: "Product D", img: "", customer: "Bob", date: "4 March", amount: 768, method: "COD", status: "Approved"},
-    {id: 12349, product: "Product E", img: "", customer: "Bob", date: "5 March", amount: 769, method: "COD", status: "Approved"},
-    {id: 12350, product: "Product F", img: "", customer: "Bob", date: "6 March", amount: 770, method: "COD", status: "Approved"},
+    {
+      id: 12345,
+      product: "Product A",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvH5zpENpwGL1yfX6oMBT-NG-Xc_aOyHdxvA&usqp=CAU",
+      customer: "Bob",
+      date: "1 March",
+      amount: 765,
+      method: "COD",
+      status: "Approved",
+    },
+    {
+      id: 12346,
+      product: "Product B",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvH5zpENpwGL1yfX6oMBT-NG-Xc_aOyHdxvA&usqp=CAU",
+      customer: "Bob",
+      date: "2 March",
+      amount: 766,
+      method: "COD",
+      status: "Approved",
+    },
+    {
+      id: 12347,
+      product: "Product C",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvH5zpENpwGL1yfX6oMBT-NG-Xc_aOyHdxvA&usqp=CAU",
+      customer: "Bob",
+      date: "3 March",
+      amount: 767,
+      method: "COD",
+      status: "Approved",
+    },
+    {
+      id: 12348,
+      product: "Product D",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvH5zpENpwGL1yfX6oMBT-NG-Xc_aOyHdxvA&usqp=CAU",
+      customer: "Bob",
+      date: "4 March",
+      amount: 768,
+      method: "COD",
+      status: "Approved",
+    },
+    {
+      id: 12349,
+      product: "Product E",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvH5zpENpwGL1yfX6oMBT-NG-Xc_aOyHdxvA&usqp=CAU",
+      customer: "Bob",
+      date: "5 March",
+      amount: 769,
+      method: "COD",
+      status: "Approved",
+    },
+    {
+      id: 12350,
+      product: "Product F",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvH5zpENpwGL1yfX6oMBT-NG-Xc_aOyHdxvA&usqp=CAU",
+      customer: "Bob",
+      date: "6 March",
+      amount: 770,
+      method: "COD",
+      status: "Approved",
+    },
   ];
 
   return (
@@ -31,19 +85,23 @@ const TransactionTable = () => {
             <TableCell className="tableCell">Amount</TableCell>
             <TableCell className="tableCell">Payment Method</TableCell>
             <TableCell className="tableCell">Status</TableCell>
-            
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.id}>
               <TableCell className="tableCell">{row.id}</TableCell>
-              <TableCell className="tableCell">{row.product}</TableCell>
+              <TableCell className="tableCell">
+                <div className="cellWrapper">
+                  <img src={row.img} alt="" className="image" />
+                  {row.product}
+                </div>
+              </TableCell>
               <TableCell className="tableCell">{row.customer}</TableCell>
               <TableCell className="tableCell">{row.date}</TableCell>
               <TableCell className="tableCell">{row.amount}</TableCell>
               <TableCell className="tableCell">{row.method}</TableCell>
-              <TableCell className="tableCell">{row.status}</TableCell>
+              <TableCell className="tableCell"><span className={`status ${row.status}`}>{row.status}</span></TableCell>
             </TableRow>
           ))}
         </TableBody>
