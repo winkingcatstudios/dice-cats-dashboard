cc1d2e4b685b31d843993591e2196b7a88381e23
winkingcat <dankercher@gmail.com>
2023-06-06 09:21:09 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />}></Route>
+              <Route path=":userID" element={<Single />}></Route>
+              <Route path="new" element={<New />}></Route>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/new/New.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./new.scss"
+
+const New = () => {
+  return (
+    <div>New</div>
+  )
+}
+
+export default New
